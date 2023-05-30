f01ae7677125c0afb171fd46c184e19015d47a71
Amel <ameladdou213@gmail.com>
2023-05-30 10:38:40 +0200

    rajout https

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 axios.defaults.baseURL =
-  "http://cookolistoba.cluster-ig3.igpolytech.fr" || "http://localhost:4000/";
+  "https://cookolistoba.cluster-ig3.igpolytech.fr" || "http://localhost:4000/";
 axios.defaults.headers.common["Authorization"] =
   "Bearer " + localStorage.getItem("token");
